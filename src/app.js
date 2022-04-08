8e9bcf3e2cda9cd084562de13a1d33c022a8bd57
kenballon <bkenn003@gmail.com>
2022-04-08 15:58:25 +0800

    updates

--- src/app.js
+++ src/app.js
@@ -1,96 +1,99 @@
-import {
-    TMDBKey
-} from './api.js';
+document.addEventListener('readystatechange', e => {
+    if(e.target.readyState === 'complete')
+    loadMovieList();        
+})
 
 
-const carouselSlider = document.querySelector('.carousel');
-const carouselSliderNetflix = document.querySelector('.netflix-carousel');
-const heroSection = document.querySelector('.hero');
+import { TMDBKey } from "./api.js";
+
+const carouselSlider = document.querySelector(".carousel");
+const carouselSliderNetflix = document.querySelector(".netflix-carousel");
+const heroSection = document.querySelector(".hero");
 
 // get api token
 // url variables
 const API = TMDBKey.keyID;
 const api_url = `https://api.themoviedb.org/3`;
-const api_img_url = 'https://image.tmdb.org/t/p/original';
+const api_img_url = "https://image.tmdb.org/t/p/original";
 
-// objects catergories from TMDB 
+// objects catergories from TMDB
 const requestCat = {
-    fetchNetflixOriginals: `/discover/tv?api_key=${API}&with_networks=213`,
-    fetchTrending: `/trending/all/week?api_key=${API}&language=en-PH`,
-    fetchTopRated: `/movie/top_rated?api_key=${API}&language=en-PH`,
-    fetchActionMovies: `/discover/movie?api_key=${API}&with_genres=28`,
-    fetchComedy: `/discover/movie?api_key=${API}&with_genres=35`,
-    fetchHorror: `/discover/movie?api_key=${API}&with_genres=27`,
-    fetchRomance: `/discover/movie?api_key=${API}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API}&with_genres=99`
-}
+  fetchNetflixOriginals: `/discover/tv?api_key=${API}&with_networks=213`,
+  fetchTrending: `/trending/all/week?api_key=${API}&language=en-PH`,
+  fetchTopRated: `/movie/top_rated?api_key=${API}&language=en-PH`,
+  fetchActionMovies: `/discover/movie?api_key=${API}&with_genres=28`,
+  fetchComedy: `/discover/movie?api_key=${API}&with_genres=35`,
+  fetchHorror: `/discover/movie?api_key=${API}&with_genres=27`,
+  fetchRomance: `/discover/movie?api_key=${API}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API}&with_genres=99`,
+};
 
 // Movies and TV Show from TMDB API
 class Movies {
+  // method to fetch data
+  async getMoviesAndTVShowsTrending() {
+    try {
+      const results = await fetch(api_url + requestCat.fetchTrending);
+      const data = await results.json();
+      const movies = data.results;
 
-    // method to fetch data
-    async getMoviesAndTVShowsTrending() {
-        try {
-            const results = await fetch(api_url + requestCat.fetchTrending);
-            const data = await results.json();
-            const movies = data.results;
-
-            return movies;
-        } catch (error) {
-            console.log(error);
-        }
+      return movies;
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async getMoviesAndTVShowsNetflixOriginal() {
-        try {
-            const results = await fetch(api_url + requestCat.fetchNetflixOriginals);
-            const data = await results.json();
-            const movies = data.results;
+  async getMoviesAndTVShowsNetflixOriginal() {
+    try {
+      const results = await fetch(api_url + requestCat.fetchNetflixOriginals);
+      const data = await results.json();
+      const movies = data.results;
 
-            return movies;
-        } catch (error) {
-            console.log(error);
-        }
+      return movies;
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
 
 // Render UI Compnonents such as Movie Card
 class RenderUI {
-    displayMoviesShows(moviesAndShows) {
+  displayMoviesShows(moviesAndShows) {
+    let result = "";
 
-        let result = '';
-
-        moviesAndShows.forEach(element => {
-            result += `
+    moviesAndShows.forEach((element) => {
+      result += `
             <div class="carousel_carousel-item" data-target="card-item">       
-                     <img src="${api_img_url + element.poster_path}" alt="${element.title || element.original_name}" width="200">                                            
+                     <img src="${api_img_url + element.poster_path}" alt="${
+        element.title || element.original_name
+      }" width="200">                                            
             </div>   
             `;
-        });
+    });
 
-        carouselSlider.insertAdjacentHTML('beforeend', result);
-    }
+    carouselSlider.insertAdjacentHTML("beforeend", result);
+  }
 
-    displayNetflixOriginal(moviesAndShows) {
+  displayNetflixOriginal(moviesAndShows) {
+    let result = "";
 
-        let result = '';
-
-        moviesAndShows.forEach(element => {
-            result += `
+    moviesAndShows.forEach((element) => {
+      result += `
             <div class="carousel_carousel-item" data-target="card-item">       
-                     <img src="${api_img_url + element.poster_path}" alt="${element.title || element.original_name}" width="200">                            
+                     <img src="${api_img_url + element.poster_path}" alt="${
+        element.title || element.original_name
+      }" width="200">                            
             </div>   
             `;
-        });
+    });
 
-        carouselSliderNetflix.insertAdjacentHTML('beforeend', result);
-    }
+    carouselSliderNetflix.insertAdjacentHTML("beforeend", result);
+  }
 
-    createHeroSection(img, title, oveview) {
+  createHeroSection(img, title, oveview) {
+    heroSection.setAttribute("style", `background-image:url(${img})`);
 
-        heroSection.setAttribute('style', `background-image:url(${img})`);
-
-        const heroSectionEl = `
+    const heroSectionEl = `
         <div class="hero_billboard-wrapper inner-width">
             <div class="billboard-title">            
                 <h1>${title}</h1>
@@ -119,48 +122,53 @@ class RenderUI {
            
         </div>
         `;
-        heroSection.insertAdjacentHTML('beforeend', heroSectionEl);
-    }
+    heroSection.insertAdjacentHTML("beforeend", heroSectionEl);
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const renderUIComponent = new RenderUI();
-    const moviesAndTV = new Movies();
 
-    moviesAndTV.getMoviesAndTVShowsTrending().then(movies => {
-        console.log(movies);
-        let i = 0;        
-        renderUIComponent.createHeroSection(api_img_url + movies[i].poster_path, (movies[i].title || movies[i].original_name), movies[i].overview);
-        renderUIComponent.displayMoviesShows(movies);
-    });
+const loadMovieList = () => {
+  const renderUIComponent = new RenderUI();
+  const moviesAndTV = new Movies();
 
-    moviesAndTV.getMoviesAndTVShowsNetflixOriginal().then(netflixOriginal => {                
-        renderUIComponent.displayNetflixOriginal(netflixOriginal);
-    })
+  moviesAndTV.getMoviesAndTVShowsTrending().then((movies) => {    
+    let i = 0;
+    console.log(movies.slice(0,5))
+    renderUIComponent.createHeroSection(
+      api_img_url + movies[i].poster_path,
+      movies[i].title || movies[i].original_name,
+      movies[i].overview
+    );
+    renderUIComponent.displayMoviesShows(movies);
+  });
 
-    // make header sticky when scroll
-    window.onscroll = function () {
-        headerScrollSticky()
-    };
-    const header = document.querySelector('.header');
-    const sticky = header.offsetTop;   
+  moviesAndTV.getMoviesAndTVShowsNetflixOriginal().then((netflixOriginal) => {
+    renderUIComponent.displayNetflixOriginal(netflixOriginal);
+  });
 
-    function headerScrollSticky() {
-        if (window.pageYOffset > sticky) {
-            header.classList.add('sticky');
-        } else {
-            header.classList.remove('sticky');
-        }
+  // make header sticky when scroll
+  window.onscroll = function () {
+    headerScrollSticky();
+  };
+  const header = document.querySelector(".header");
+  const sticky = header.offsetTop;
+
+  function headerScrollSticky() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
     }
+  }
 
-    // show menu on mobile view
-    const menu = document.getElementById('mobile-menu');
-    const menuOverlay = document.querySelector('.menu-pop-container');
+  // show menu on mobile view
+  const menu = document.getElementById("mobile-menu");
+  const menuOverlay = document.querySelector(".menu-pop-container");
 
-    menu.addEventListener('click',() => {
-        menuOverlay.classList.add('popup-menu');
-    })
-    menuOverlay.addEventListener('click',()=>{
-        menuOverlay.classList.remove('popup-menu');
-    })
-});
+  menu.addEventListener("click", () => {
+    menuOverlay.classList.add("popup-menu");
+  });
+  menuOverlay.addEventListener("click", () => {
+    menuOverlay.classList.remove("popup-menu");
+  });
+};
